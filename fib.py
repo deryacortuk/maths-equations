6a33f654c946ec57668bf0ac1e09865f945803ac
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2021-04-10 20:35:38 +0300

    Generator
    Memory is saved as the items are produced when required, unlike normal Python functions. This fact becomes very important when you need to create a huge number of iterators. This is also considered as the biggest advantage of generators.

---
+++ fib.py
@@ -0,0 +1,15 @@
+def fibonacci():
+    i = 1
+    j= 1
+    yield i
+    yield j
+
+    while True:
+
+        i,j = j, i+j
+        yield j
+
+for number in fibonacci():
+    if(number>100):
+        break
+    print(number)
